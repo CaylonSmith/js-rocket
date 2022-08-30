edf918e422670bd529b5e3074d62f5a8e0fcab3d
Caylow27 <91961508+CaylonSmith@users.noreply.github.com>
2022-08-30 12:33:27 +0200

    made some car stuff happen

---
+++ first.js
@@ -0,0 +1,30 @@
+
+    var car = {
+      make: "VW",
+      type: "polo",
+      color: "blue",
+      isTurnedOn: false,
+      numOfWheels: 4,
+      seats: ['seat1', 'seat2', 'seat3', 'seat4'],
+
+      turnedOn : function(){
+        this.isTurnedOn= true;
+
+      },
+
+      fly : function(){
+        alert('fly');
+      },
+
+      switchCar : function(isON){
+console.log('turn car'+isON);
+if(isON == true){
+    this.isTurnedOn = true;
+}
+else{
+    this.isTurnedOn=false;
+}
+      }
+    };
+
+    console.log("hello there sweeet cheeks");
